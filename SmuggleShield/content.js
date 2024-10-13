b71aa53cae9bdf3031d82ba32e295840dd5ef63f
Dhiraj Mishra <mishra.dhiraj95@gmail.com>
2024-10-13 19:14:43 +0400

    update content.js

--- SmuggleShield/content.js
+++ SmuggleShield/content.js
@@ -45,6 +45,11 @@ class HTMLSmugglingBlocker {
     ];
     this.threshold = 4;
     this.setupListeners();
+
+    this.suspiciousPatterns = this.suspiciousPatterns.map(({pattern, weight}) => ({
+      pattern: new RegExp(pattern, 'is'),
+      weight
+    }));
   }
 
   setupListeners() {
@@ -81,12 +86,13 @@ class HTMLSmugglingBlocker {
     let score = 0;
     const detectedPatterns = [];
 
-    this.suspiciousPatterns.forEach(({pattern, weight}) => {
+    for (const {pattern, weight} of this.suspiciousPatterns) {
       if (pattern.test(htmlContent)) {
         score += weight;
         detectedPatterns.push(pattern.toString());
+        if (score >= this.threshold) break; // Early exit if threshold is reached
       }
-    });
+    }
 
     if (score >= this.threshold) {
       console.log("HTML Smuggling Blocker: Suspicious content detected");
